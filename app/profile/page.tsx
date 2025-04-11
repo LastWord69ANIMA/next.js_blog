70fba769cade268bacc3de3af5f13fc7dfb52b36
LastWord69ANIMA <hase715.aqua@gmail.com>
2025-04-11 22:44:24 +0900

    自己紹介内容を変更

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -7,7 +7,11 @@ import { Flex} from "@chakra-ui/react";
 import Header from "@/app/src/components/Header"
 import Footer from '@/app/src/components/Footer';
 
-{/*　次回、コンポーネントにして、読みやすく且つサイズの調整・CSSの有効活用　*/}
+{/*次回、
+    ・項目のコンポーネント化
+    ・サイズの調整
+    ・職歴と資格欄を、箱内にて箇条書き
+*/}
 export default function Profile() {
     return(
         <div
@@ -18,7 +22,7 @@ export default function Profile() {
             <div>
                 
                 <Flex className={styles.home}>
-                --プロフィール--
+                --Profile--
                 </Flex>
                 <Flex className={styles.home}>
                 HN:LastWord69ANIMA
@@ -27,163 +31,39 @@ export default function Profile() {
                 FROM:Japan
                 </Flex>
                 <Flex className={styles.home}>
-                HOBBY:
+                ↓Work history↓
                 </Flex>
                 <Flex className={styles.home}>
-                1.Programing
+                2025/4～Now:
                 </Flex>
                 <Flex className={styles.home}>
-                2.Reading
-                </Flex>
-                <Flex className={styles.home}>
-                3.Work out
-                etc...
+                PERSOL CROSS TECHNOLOGY CO., LTD.
                 </Flex>
 
                 <Flex className={styles.home}>
-                Skill:
+                ↓Skill↓
                 </Flex>
 
-                <div className={styles.hidden}>
-                    <Link href="https://github.com/LastWord69ANIMA/next.js_blog"
-                        className={styles.logohidden}
-                    >
-                        <Image
-                            src={"/typescript-icon.svg"}
-                            alt={"Picture of TypeScript"}
-                            width={100}
-                            height={100}
-                        ></Image>
-                    </Link>
+                <div className={styles.home}>
+                    <Flex>
+                        2021/6 実用英語技能検定準一級
+                    </Flex>
 
-                    <Link href="https://github.com/LastWord69ANIMA/Git-command-tutorial"
-                        className={styles.logohidden}
-                    >
-                        <Image
-                            src={"/php.svg"}
-                            alt={"Picture of PHP"}
-                            width={100}
-                            height={100}
-                        ></Image>
-                    </Link>
+                    <Flex>
+                        2023/6 日商簿記2級
+                    </Flex>
 
-                    <Link href="https://github.com/LastWord69ANIMA/Twiiter_api"
-                        className={styles.logohidden}
-                    >
-                        <Image
-                            src={"/python.svg"}
-                            alt={"Picture of Python"}
-                            width={100}
-                            height={100}
-                        ></Image>
-                    </Link>
+                    <Flex>
+                        2024/3 基本情報技術者試験
+                    </Flex>
 
-                </div>
+                    <Flex>
+                        2024/10 LPIC-1
+                    </Flex>
 
-                <Flex className={styles.home}>
-                Framework/Library:
-                </Flex>
-
-                <div className={styles.hidden}>
-                    <Link href="https://github.com/LastWord69ANIMA/next.js_blog"
-                        className={styles.logohidden}
-                    >
-                        <Image
-                            src={"/nextjs-icon.svg"}
-                            alt={"Picture of Next.js"}
-                            width={100}
-                            height={100}
-                        ></Image>
-                    </Link>
-
-                    <Link href="https://github.com/LastWord69ANIMA/next.js_blog"
-                        className={styles.logohidden}
-                    >
-                        <Image
-                            src={"/react.svg"}
-                            alt={"Picture of react"}
-                            width={100}
-                            height={100}
-                        ></Image>
-                    </Link>
-                </div>
-
-                <Flex className={styles.home}>
-                Others:
-                </Flex>
-
-                <div className={styles.hidden}>
-                    <Link href="https://github.com/LastWord69ANIMA/php-docker"
-                        className={styles.logohidden}
-                    >
-                        <Image
-                            src={"/docker-icon.svg"}
-                            alt={"Picture of Docker"}
-                            width={100}
-                            height={100}
-                        ></Image>
-                    </Link>
-
-                    <Link href="https://github.com/LastWord69ANIMA/next.js_blog"
-                        className={styles.logohidden}
-                    >
-                        <Image
-                            src={"/vercel.svg"}
-                            alt={"Picture of vercel"}
-                            width={100}
-                            height={100}
-                        ></Image>
-                    </Link>
-
-                    <Link href="https://github.com/LastWord69ANIMA/Git-command-tutorial"
-                        className={styles.logohidden}
-                    >
-                        <Image
-                            src={"/mysql.svg"}
-                            alt={"Picture of Mysql"}
-                            width={100}
-                            height={100}
-                        ></Image>
-                    </Link>
-                </div>
-
-                <Flex className={styles.home}>
-                Studying:
-                </Flex>
-
-                <div className={styles.hidden}>
-                <Link href="https://kotlinlang.org/"
-                        className={styles.logohidden}
-                    >
-                        <Image
-                            src={"/kotlin.svg"}
-                            alt={"Picture of kotlin"}
-                            width={100}
-                            height={100}
-                        ></Image>
-                    </Link>
-
-                    <Link href="https://developer.android.com/studio?hl=ja"
-                        className={styles.logohidden}
-                    >
-                        <Image
-                            src={"/android-studio.svg"}
-                            alt={"Picture of android studio"}
-                            width={100}
-                            height={100}
-                        ></Image>
-                    </Link>
-
-                    <Link href="https://learn.microsoft.com/ja-jp/cpp/c-language/?view=msvc-170"
-                        className={styles.logohidden}
-                    >
-                        <Image
-                            src={"/c.svg"}
-                            alt={"Picture of C言語"}
-                            width={100}
-                            height={100}
-                        ></Image>
-                    </Link>
+                    <Flex>
+                        2024/12 CCNA(200-301)
+                    </Flex>
                 </div>
 
             </div>
